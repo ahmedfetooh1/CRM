e5988815f8a4cc3380039f2534f0dd0814724b34
AhmedAboElFetooh <ahmedfetooh54@gmail.com>
2025-07-04 16:18:03 +0300

    add view button to dashboard and show record detail

--- webapp/urls.py
+++ webapp/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('my-login/',views.my_login,name='login'),
     path('dashboard/',views.dashboard,name='dashboard'),
     path('logout/',views.my_logout,name='logout'),
-    path('create-record',views.create_record,name='createrecord')
+    path('create-record',views.create_record,name='createrecord'),
+    path('view/<int:id>/',views.view_record,name='view_record'),
 ]
--- webapp/views.py
+++ webapp/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render ,redirect
+from django.shortcuts import render ,redirect , get_object_or_404
 from .forms import CreateUserForm , LoginForm ,CreateRecordForm
 from django.contrib.auth import authenticate , login , logout
 from django.contrib.auth.decorators import login_required
@@ -73,3 +73,13 @@ def create_record(request):
         'form':form
     }
     return render(request,'web/create-record.html',context)
+
+@login_required(login_url='login')
+def view_record(request,id):
+    all_records = get_object_or_404(Record ,id=id)
+
+    context = {
+        'record':all_records ,
+    }
+
+    return render(request,'web/view_record.html',context)
